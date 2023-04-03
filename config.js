3a4c7e3103642a95fba3dccfb09176a7ce03c517
小谢吃不胖 <xiebeihao@gzaoyima.com>
2023-04-03 15:08:00 +0800

    first

---
+++ config.js
@@ -0,0 +1,25 @@
+// /** @format */
+// module.exports = {
+// 	themeConfig: {
+// 		nav: [
+// 			{
+// 				text: "Languages",
+// 				items: [
+// 					{
+// 						text: "Group1",
+// 						items: [
+// 							/*  */
+// 						],
+// 					},
+// 					{
+// 						text: "Group2",
+// 						items: [
+// 							/*  */
+// 						],
+// 					},
+// 				],
+// 			},
+// 		],
+// 		sidebar: ["/", "/page-a", ["/page-b", "Explicit link text"]],
+// 	},
+// }
