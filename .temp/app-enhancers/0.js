602e43de7d7184b2d66410aeeff74b1d53d78734
小谢吃不胖 <xiebeihao@gzaoyima.com>
2023-04-04 10:43:08 +0800

    更新

--- .temp/app-enhancers/0.js
+++
@@ -1,2 +0,0 @@
-import "D:\\dev\\nvm\\v12.13.1\\node_modules\\vuepress\\node_modules\\@vuepress\\core\\lib\\node\\internal-plugins\\style\\client.js"
-export default {}
--- .temp/app-enhancers/global-components-1.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-Vue.component("CodeBlock", () => import("D:\\dev\\nvm\\v12.13.1\\node_modules\\vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\CodeBlock"))
-Vue.component("Badge", () => import("D:\\dev\\nvm\\v12.13.1\\node_modules\\vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
-Vue.component("CodeGroup", () => import("D:\\dev\\nvm\\v12.13.1\\node_modules\\vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\CodeGroup"))
-
-
-export default {}
